84e7f7a634789cc966950a5f5b7e910b6695976a
Tony Phan <77694038+tonyphan138@users.noreply.github.com>
2022-05-23 12:08:00 -0600

    Add files via upload

---
+++ CS430/LoadTable.sql
@@ -0,0 +1,33 @@
+
+SELECT 'LOADING author' as 'INFO';
+source CS430/Module7/load_author.dump;
+
+SELECT 'LOADING phone' as 'INFO';
+source CS430/Module7/load_phone.dump;
+
+SELECT 'LOADING publisher' as 'INFO';
+source CS430/Module7/load_publisher.dump;
+
+SELECT 'LOADING book' as 'INFO';
+source CS430/Module7/load_book.dump;
+
+SELECT 'LOADING member' as 'INFO';
+source CS430/Module7/load_member.dump;
+
+SELECT 'LOADING written_by' as 'INFO';
+source CS430/Module7/load_written_by.dump;
+
+SELECT 'LOADING author_phone' as 'INFO';
+source CS430/Module7/load_author_phone.dump;
+
+SELECT 'LOADING publisher_phone' as 'INFO';
+source CS430/Module7/load_publisher_phone.dump;
+
+SELECT 'LOADING library' as 'INFO';
+source CS430/Module7/load_library.dump;
+
+SELECT 'LOADING located_at' as 'INFO';
+source CS430/Module7/load_located_at.dump;
+
+SELECT 'LOADING borrowed' as 'INFO';
+source CS430/Module7/load_borrowed.dump;
